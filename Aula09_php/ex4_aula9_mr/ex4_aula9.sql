c14396be0f05c44c7410cbe5de8628663ff539cc
enrico_876 <enricosafonso36@gmail.com>
2025-11-03 17:09:38 -0300

    Update SQL file generation time and add database creationarrumando

--- Aula09_php/ex4_aula9_mr/ex4_aula9.sql
+++ Aula09_php/ex4_aula9_mr/ex4_aula9.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30/10/2025 às 20:23
+-- Tempo de geração: 03/11/2025 às 21:00
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `ex4_aula9`
 --
+CREATE DATABASE IF NOT EXISTS `ex4_aula9` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `ex4_aula9`;
 
 -- --------------------------------------------------------
 
